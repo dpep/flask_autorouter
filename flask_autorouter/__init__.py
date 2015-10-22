9e09ff45c3d6592fbe480099ef41cfa3956e1041
dpepper <pepper.daniel@gmail.com>
2015-10-21 19:39:59 -0700

    expand http method handling to include put, delete, and options

--- flask_autorouter/__init__.py
+++ flask_autorouter/__init__.py
@@ -5,45 +5,48 @@ __version__ = "0.1.1"
 import os
 import imp
 import json
-from bson import json_util
 import urllib
+
+from bson import json_util
 import flask
 
 
-def generate_urls(app, src_dir, base_url, default_methods=['GET'], auth_fn=None):
+def generate_urls(app, src_dir, base_url='/', default_methods=['GET'], auth_fn=None):
+    assert type(app) == flask.Flask
     assert base_url.startswith('/')
     base_url = base_url.rstrip('/')
 
     if os.path.isdir(src_dir):
         src_dir = os.path.abspath(src_dir)
     else:
-        src_dir = '%s/%s' % (app.config['ROOT_PATH'], src_dir)
+        src_dir = os.path.join(app.config['ROOT_PATH'], src_dir)
 
     assert os.path.isdir(src_dir)
     assert src_dir.startswith(app.config['ROOT_PATH'])
 
-    def generate_view_fn(get_fn, post_fn):
+    def generate_view_fn(http_fns):
         def view_fn(*args, **kwargs):
-            if flask.request.method == 'POST':
-                res = view_fn.post_fn(*args, **kwargs)
-            else:
-                # GET
-                res = view_fn.get_fn(*args, **kwargs)
+            fn = view_fn.http_fns.get(flask.request.method)
 
-            if None == res:
-                res = ('', 204)  # Empty Response Code
-            elif type(res) in [list, dict]:
-                # auto-package results into json
-                res = flask.Response(
-                    json.dumps(res, default=json_util.default),
-                    mimetype='application/json',
-                )
+            if fn:
+                res = fn(*args, **kwargs)
+
+                if None == res:
+                    res = ('', 204)  # Empty Response Code
+                elif type(res) in [list, dict]:
+                    # auto-package results into json
+                    res = flask.Response(
+                        json.dumps(res, default=json_util.default),
+                        mimetype='application/json',
+                    )
+            else:
+                res = ('', 405)  # Method Not Allowed
 
             return res
-        view_fn.get_fn = get_fn
-        view_fn.post_fn = post_fn
 
+        view_fn.http_fns = http_fns
         return view_fn
+
 
     paths = {}
     for path, subdirs, filenames in os.walk(src_dir):
@@ -53,38 +56,42 @@ def generate_urls(app, src_dir, base_url, default_methods=['GET'], auth_fn=None)
                 continue
 
             mod_name = '%s/%s' % (path[len(app.config['ROOT_PATH']):], filename[:-3])
-            mod_path = os.path.abspath(path + '/' + filename)
+            mod_path = os.path.abspath(os.path.join(path, filename))
             module = imp.load_source(mod_name, mod_path)
 
             defaults = getattr(module, 'DEFAULTS', {})
             public_view = getattr(module, 'PUBLIC', False)
 
-            get_fn = getattr(module, 'get', None)
-            post_fn = getattr(module, 'post', None)
-            view_fn = getattr(module, 'view', None)
+            http_fns = {}
+            for method in ['VIEW', 'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']:
+                fn = getattr(module, method, None)
 
-            if not any([view_fn, get_fn, post_fn]):
-                raise Exception('endpoint missing magic view() or get()/post() functions: ' + mod_path)
+                if fn:
+                    if getattr(module, method.lower(), None):
+                        raise Exception('endpoint has both %s() and %s(): %s' % (method, method.lower(), mod_path))
+                else:
+                    fn = getattr(module, method.lower(), None)
 
-            if view_fn and (get_fn or post_fn):
-                raise Exception('endpoints can have either view() or get()/post() but not both: ' + mod_path)
+                if fn:
+                    if not callable(fn):
+                        raise Exception('invalid http method function: %s: %s' % (method, mod_path))
 
-            if view_fn:
-                if not callable(view_fn):
-                    raise Exception('invalid view function: %s' % mod_path)
-            else:
-                view_fn = generate_view_fn(get_fn, post_fn)
+                    http_fns[method] = fn
 
-            if not public_view:
+
+            if not http_fns:
+                raise Exception('invalid endpoint, missing http function: ' + mod_path)
+
+            if http_fns.get('VIEW') and (http_fns.get('GET') or http_fns.get('POST')):
+                raise Exception('invalid endpoints, use either view() or get()/post() but not both: ' + mod_path)
+
+            view_fn = generate_view_fn(http_fns)
+
+            if not public_view and auth_fn:
                 view_fn = auth_fn(view_fn)
 
             # determine which methods to use for this endpoint
-            methods = set(getattr(module, 'METHODS', []))
-
-            if get_fn:
-                methods.add('GET')
-            if post_fn:
-                methods.add('POST')
+            methods = set(getattr(module, 'METHODS', [ k for k, v in http_fns.items() if v ]))
 
             methods = methods or default_methods
 
@@ -94,7 +101,6 @@ def generate_urls(app, src_dir, base_url, default_methods=['GET'], auth_fn=None)
                 url_paths = getattr(module, 'PATHS')
             else:
                 # determine this endpoint's url, based on it's path
-                # prefix = '%s/%s' % (base_url, path[len(src_dir):])
                 url = '%s/%s/%s' % (
                     base_url,
                     path[len(src_dir):],
@@ -114,15 +120,25 @@ def generate_urls(app, src_dir, base_url, default_methods=['GET'], auth_fn=None)
                 paths[url] = mod_name
 
 
-def list_routes(app):
+def list_routes(app, verbose=False):
+    assert type(app) == flask.Flask
+
     output = []
     for rule in app.url_map.iter_rules():
         if rule.endpoint == 'static':
             continue
 
-        line = urllib.unquote("{:30s}   {}".format(rule.rule, rule.endpoint))
-        if len(line) > 70:
-            line = line[:70] + '...'
+        if verbose:
+            methods = sorted([ x for x in rule.methods if x not in ['HEAD', 'OPTIONS'] ])
+        else:
+            methods = ''
+        line = urllib.unquote("{:30s}\t{:30s}\t{}".format(rule.rule, rule.endpoint, methods))
+
+        if not verbose:
+            # truncate long lines
+            if len(line) > 70:
+                line = line[:70] + '...'
+
         output.append(line)
 
     output = sorted(output)
